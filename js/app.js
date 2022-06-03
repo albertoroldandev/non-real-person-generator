37f3aa891c0e2f294b3bb139663cb54f734f69f0
albertoroldan11 <105441444+albertoroldan11@users.noreply.github.com>
2022-06-03 12:33:00 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,146 @@
+let maleNames = ["Antonio","Manuel","Jose","Francisco","David","Juan","Javier","Jose Antonio","Daniel","Francisco Javier","Jose Luis","Carlos","Jesus","Alejandro","Miguel","Jose Manuel","Rafael","Miguel Angel","Pablo","Pedro","Angel","Sergio","Jose Maria","Fernando","Jorge","Luis","Alberto","Alvaro","Juan Carlos","Adrian","Diego","Juan Jose","Raul","Ivan","Juan Antonio","Ruben","Enrique","Oscar","Ramon","Andres","Juan Manuel","Vicente","Santiago","Joaquin","Mario","Victor","Eduardo","Roberto","Jaime","Francisco Jose","Marcos","Hugo","Ignacio","Jordi","Alfonso","Ricardo","Salvador","Guillermo","Marc","Gabriel","Mohamed","Emilio","Gonzalo","Martin","Jose Miguel","Julio","Julian","Tomas","Nicolas","Agustin","Jose Ramon","Samuel","Ismael","Cristian","Lucas","Joan","Felix","Aitor","Hector","Iker","Alex","Juan Francisco","Jose Carlos","Josep","Sebastian","Cesar","Mariano","Alfredo","Domingo","Mateo","Jose Angel","Rodrigo","Victor Manuel","Felipe","Jose Ignacio","Luis Miguel","Jose Francisco","Xavier","Juan Luis","Albert"]
+let femaleNames = ["Maria Carmen","Maria","Carmen","Ana Maria","Maria Pilar","Laura","Josefa","Isabel","Maria Dolores","Maria Teresa","Ana","Marta","Cristina","Maria Angeles","Lucia","Maria Isabel","Maria Jose","Francisca","Antonia","Dolores","Sara","Paula","Elena","Maria Luisa","Raquel","Rosa Maria","Manuela","Pilar","Concepcion","Maria Jesus","Mercedes","Julia","Beatriz","Alba","Silvia","Nuria","Irene","Rosario","Patricia","Juana","Teresa","Encarnacion","Andrea","Rocio","Montserrat","Monica","Alicia","Maria Mar","Sandra","Sonia","Marina","Rosa","Angela","Susana","Natalia","Yolanda","Margarita","Sofia","Claudia","Maria Josefa","Eva","Carla","Maria Rosario","Inmaculada","Maria Mercedes","Ana Isabel","Noelia","Esther","Veronica","Nerea","Carolina","Daniela","Ines","Eva Maria","Maria Victoria","Angeles","Miriam","Lorena","Maria Rosa","Ana Belen","Maria Elena","Martina","Victoria","Maria Concepcion","Amparo","Alejandra","Maria Antonia","Lidia","Celia","Catalina","Maria Nieves","Fatima","Ainhoa","Olga","Consuelo","Clara","Gloria","Maria Cristina","Maria Soledad","Adriana"]
+let surNames = ["Garcia","Rodriguez","Gonzalez","Fernandez","Lopez","Martinez","Sanchez","Perez","Gomez","Martin","Jimenez","Hernandez","Ruiz","Diaz","Moreno","Mu\xf1oz","Alvarez","Romero","Gutierrez","Alonso","Navarro","Torres","Dominguez","Ramos","Vazquez","Ramirez","Gil","Serrano","Morales","Molina","Blanco","Suarez","Castro","Ortega","Delgado","Ortiz","Marin","Rubio","Nu\xf1ez","Sanz","Medina","Iglesias","Castillo","Cortes","Garrido","Santos","Guerrero","Lozano","Cano","Mendez","Cruz","Prieto","Flores","Herrera","Pe\xf1a","Leon","Marquez","Cabrera","Gallego","Calvo","Vidal","Campos","Reyes","Vega","Fuentes","Carrasco","Diez","Aguilar","Caballero","Nieto","Santana","Pascual","Herrero","Vargas","Gimenez","Montero","Hidalgo","Lorenzo","Santiago","Iba\xf1ez","Duran","Benitez","Ferrer","Arias","Mora","Carmona","Vicente","Rojas","Soto","Crespo","Roman","Pastor","Velasco","Parra","Saez","Moya","Bravo","Soler","Gallardo","Rivera"]
+let maleImages = new Array();
+maleImages[0] = "./img/avatars/male/avatar-0b7c6c9b3f59dab6e750d884ce9ea5fd.jpg";
+maleImages[1] = "./img/avatars/male/avatar-0cd281f26e17c42db83d2c3a6b58f45c.jpg";
+maleImages[2] = "./img/avatars/male/avatar-0e4bd235be6d59fffdaf5a7e7c970197.jpg";
+maleImages[3] = "./img/avatars/male/avatar-0fb0fc0e39323fabf7e342abfe5d1053.jpg";
+maleImages[4] = "./img/avatars/male/avatar-2b49e5c973bff2408c70e31655af3e36.jpg";
+maleImages[5] = "./img/avatars/male/avatar-2b8518afdbc9b99a369c44c6604a77b7.jpg";
+maleImages[6] = "./img/avatars/male/avatar-3a1cdaa9097df95ee997382ecc25123b.jpg";
+maleImages[7] = "./img/avatars/male/avatar-3b382c27c24f971c1f62eb40ea70c5ab.jpg";
+maleImages[8] = "./img/avatars/male/avatar-6ea0ea3ff1575937352b755618bf3164.jpg";
+maleImages[9] = "./img/avatars/male/avatar-8e58d9a7e6ef189696a25568e2ab3fea.jpg";
+maleImages[10] = "./img/avatars/male/avatar-9a8ee3acba56b617f92af3de97f0afaa.jpg";
+maleImages[11] = "./img/avatars/male/avatar-9d78a1071d806b84b9ae3bf376db3c96.jpg";
+maleImages[12] = "./img/avatars/male/avatar-9e024b615f93af78e6912e7232d3eed0.jpg";
+maleImages[13] = "./img/avatars/male/avatar-14b894133305e84e840f82153fd7417a.jpg";
+maleImages[14] = "./img/avatars/male/avatar-58d8781a290927d7f4654094ff59d35f.jpg";
+maleImages[15] = "./img/avatars/male/avatar-79c48ec5719bc5bbe2f9ce66e5a861c8.jpg";
+maleImages[16] = "./img/avatars/male/avatar-84c3f1fc8aaf614c5a9b340f01c7ff46.jpg";
+maleImages[17] = "./img/avatars/male/avatar-86cb291b736ec1a79150c915691ef292.jpg";
+maleImages[18] = "./img/avatars/male/avatar-246ea0ba40119d5936661b21a88aa387.jpg";
+maleImages[19] = "./img/avatars/male/avatar-531b541ed2ac044bafedb5cdb58c46dd.jpg";
+maleImages[20] = "./img/avatars/male/avatar-657d84bdf1e43a40cffea9c3f5f622b5.jpg";
+maleImages[21] = "./img/avatars/male/avatar-687e41c8d2831c0ab1671825ce6cb20e.jpg";
+maleImages[22] = "./img/avatars/male/avatar-702ef26585960e3e29ad19af5c1ba914.jpg";
+maleImages[23] = "./img/avatars/male/avatar-765ea0b557fd1bfdb644e403642e2ede.jpg";
+maleImages[24] = "./img/avatars/male/avatar-845df8db159c3e354fdc50a0df0be07d.jpg";
+maleImages[25] = "./img/avatars/male/avatar-2037ea0d0fe6136b47d744c68cb087ee.jpg";
+maleImages[26] = "./img/avatars/male/avatar-2449a77e02666963717b6ab4766c95e6.jpg";
+maleImages[27] = "./img/avatars/male/avatar-6578ce67d2bcb0df9697c2a212d325bc.jpg";
+maleImages[28] = "./img/avatars/male/avatar-51113a6693fd765ca8518c8818343615.jpg";
+maleImages[29] = "./img/avatars/male/avatar-62769dc9499540908230dcdcedf39a54.jpg";
+maleImages[30] = "./img/avatars/male/avatar-741513303a9f12148bafbcf86048264a.jpg";
+maleImages[31] = "./img/avatars/male/avatar-a5046e9b024177d7a7af86915f4a6373.jpg";
+maleImages[32] = "./img/avatars/male/avatar-aa871890973fcca3f49d0f4cc3657567.jpg";
+maleImages[33] = "./img/avatars/male/avatar-aac257c9b1cb71e0991a1c75033355d2.jpg";
+maleImages[34] = "./img/avatars/male/avatar-b1adc5f0ca271ee93711a2412bbe4241.jpg";
+maleImages[35] = "./img/avatars/male/avatar-b2effd62ba06023386f0da09d2d052e8.jpg";
+maleImages[36] = "./img/avatars/male/avatar-b5d4b3db91d4d3d0a2cc0ce5d325cd10.jpg";
+maleImages[37] = "./img/avatars/male/avatar-b56ccbe5450d6f84e25cc9aaf7a84504.jpg";
+maleImages[38] = "./img/avatars/male/avatar-b7374ee1edec4996b9fccafd4237d864.jpg";
+maleImages[39] = "./img/avatars/male/avatar-b99342995fbfd4dfe8f90b4ac38c0f03.jpg";
+maleImages[40] = "./img/avatars/male/avatar-bf0e66c6d08bea275e23772ebac96ced.jpg";
+maleImages[41] = "./img/avatars/male/avatar-cf2198412289620eff6d6063015d6c70.jpg";
+maleImages[42] = "./img/avatars/male/avatar-d75c8e510970b6530ec7b994c7af44f9.jpg";
+maleImages[43] = "./img/avatars/male/avatar-d7177cc5ad27d6da1263e23681f34b1e.jpg";
+maleImages[44] = "./img/avatars/male/avatar-ddb8873d5983ae13d85a6889a90698b3.jpg";
+maleImages[45] = "./img/avatars/male/avatar-e24ef3a7a41532de2c62046e6e15109a.jpg";
+maleImages[46] = "./img/avatars/male/avatar-e53ae5d34f3e78415db8aaa804f20d23.jpg";
+maleImages[47] = "./img/avatars/male/avatar-229486d4eb476662193a8a5940b491a2.jpg";
+maleImages[48] = "./img/avatars/male/avatar-f3daee166348750438039aa0ad7fc5c2.jpg";
+maleImages[49] = "./img/avatars/male/avatar-f36083cc46506a4af7a6c536a0c3785a.jpg";
+let femaleImages = new Array();
+femaleImages[0] = "./img/avatars/female/avatar-0e12a3f5b8c7ab203e8780bd3c1184dd.jpg";
+femaleImages[1] = "./img/avatars/female/avatar-0ede93dcd1147628f9e74110ec70ce65.jpg";
+femaleImages[2] = "./img/avatars/female/avatar-0f39e66d4855f3f40c196ba832d2ff17.jpg";
+femaleImages[3] = "./img/avatars/female/avatar-01cce34b41f813dd46c5815627307cc4.jpg";
+femaleImages[4] = "./img/avatars/female/avatar-02c4b96c0eae739d0b52a0f8dece7d75.jpg";
+femaleImages[5] = "./img/avatars/female/avatar-6eb5b6cb77f59b4651cdbc47fc12f59e.jpg";
+femaleImages[6] = "./img/avatars/female/avatar-7b429da8107d594f7d12400de2bfae85.jpg";
+femaleImages[7] = "./img/avatars/female/avatar-7c77dfbcc0f5f797624e1f62c4e44608.jpg";
+femaleImages[8] = "./img/avatars/female/avatar-8b3092fc3b1fa973f64c16eb43e3e0b8.jpg";
+femaleImages[9] = "./img/avatars/female/avatar-8f85846b27733af48806ac1525d25c5f.jpg";
+femaleImages[10] = "./img/avatars/female/avatar-9c2cbc8424a61abc9e6934aa8601e990.jpg";
+femaleImages[11] = "./img/avatars/female/avatar-18ebbc7ebf867fe868ea34e4c904a670.jpg";
+femaleImages[12] = "./img/avatars/female/avatar-20bd45dac5a4477efb72074eb4910148.jpg";
+femaleImages[13] = "./img/avatars/female/avatar-30d0144a968ddd2d6df158ec48396129.jpg";
+femaleImages[14] = "./img/avatars/female/avatar-31b9372f2679d9acc6e417471a58da28.jpg";
+femaleImages[15] = "./img/avatars/female/avatar-51f277672b3ce96109dced15bd9d494c.jpg";
+femaleImages[16] = "./img/avatars/female/avatar-73c1cf89e6b8b8ffd4a5e9816a2590b7.jpg";
+femaleImages[17] = "./img/avatars/female/avatar-93b322983496c6df05e46795f487bbf9.jpg";
+femaleImages[18] = "./img/avatars/female/avatar-98de3f6e9e84df893067209c9f2cd9b2.jpg";
+femaleImages[19] = "./img/avatars/female/avatar-0124a367856ed46a52719b35f1c766c3.jpg";
+femaleImages[20] = "./img/avatars/female/avatar-203d9b86f3909ddbb1597f246555de29.jpg";
+femaleImages[21] = "./img/avatars/female/avatar-223ca9212b8a60225ae2ca8007ff3c4e.jpg";
+femaleImages[22] = "./img/avatars/female/avatar-785d98c5c448f8566531f34df4927266.jpg";
+femaleImages[23] = "./img/avatars/female/avatar-2090c2abf3ace161b6c819dff01e2386.jpg";
+femaleImages[24] = "./img/avatars/female/avatar-5613b112fc84f66ec9c3100860eadbe8.jpg";
+femaleImages[25] = "./img/avatars/female/avatar-45792fdad262e71b45a8da47b4e840a4.jpg";
+femaleImages[26] = "./img/avatars/female/avatar-128154bfef4154fe6c9ec6a85107182f.jpg";
+femaleImages[27] = "./img/avatars/female/avatar-443944e502b5551299aa4123c33599de.jpg";
+femaleImages[28] = "./img/avatars/female/avatar-84189140418f995e55483cc553fef22e.jpg";
+femaleImages[29] = "./img/avatars/female/avatar-105541799534c5aad9b8b79afebf48e9.jpg";
+femaleImages[30] = "./img/avatars/female/avatar-a2421f6a0296386e3a6cd97414126fb6.jpg";
+femaleImages[31] = "./img/avatars/female/avatar-ab22c3b5340520e4e26e15360f2ba6e1.jpg";
+femaleImages[32] = "./img/avatars/female/avatar-af3532dd6a04c4db75c5ec24d48f2e89.jpg";
+femaleImages[33] = "./img/avatars/female/avatar-b3fbc0d0ad6c7e0cf11da9960a1b2ac2.jpg";
+femaleImages[34] = "./img/avatars/female/avatar-b4b8e7af6932e46cb59c5c0e4f6579ac.jpg";
+femaleImages[35] = "./img/avatars/female/avatar-b6b87d1aa77ba74570cb3205ff715867.jpg";
+femaleImages[36] = "./img/avatars/female/avatar-b3989f28775171bececd323b0c0b1491.jpg";
+femaleImages[37] = "./img/avatars/female/avatar-bcce0356413848b947bca14088743fbc.jpg";
+femaleImages[38] = "./img/avatars/female/avatar-c8ba9ab82675e2c1cdb2717a9a734d44.jpg";
+femaleImages[39] = "./img/avatars/female/avatar-c95b9d1f9d64ee38e635c786c5bf4866.jpg";
+femaleImages[40] = "./img/avatars/female/avatar-ccef0f281f6a6604723030d85043cb8d.jpg";
+femaleImages[41] = "./img/avatars/female/avatar-d02cb171fa50f832cecbafc04ef21c2a.jpg";
+femaleImages[42] = "./img/avatars/female/avatar-d5afeda308c9fd7d611d89b0ea2578bf.jpg";
+femaleImages[43] = "./img/avatars/female/avatar-d1034b268c5e5c280a92876868c21647.jpg";
+femaleImages[44] = "./img/avatars/female/avatar-daea4c802bf744eb184538befa016711.jpg";
+femaleImages[45] = "./img/avatars/female/avatar-e18c4327ab9416fb9de7311b73669d27.jpg";
+femaleImages[46] = "./img/avatars/female/avatar-e75b9eb252cba29a78c91186eae3c237.jpg";
+femaleImages[47] = "./img/avatars/female/avatar-f38f70c76cbae928a67a971158745cb1.jpg";
+femaleImages[48] = "./img/avatars/female/avatar-fc0867048b45b2181f10fd695d756f70.jpg";
+femaleImages[49] = "./img/avatars/female/avatar-ff3e1835dd3dffbe7ecd0138e3b8707e.jpg";
+
+function generate_person(){
+    let rn = Math.floor(Math.random()*100);
+    let rs1 = Math.floor(Math.random()*100);
+    let rs2 = Math.floor(Math.random()*100);
+    let ri = Math.floor(Math.random()*50);
+    let rr = Math.floor(Math.random()*2);
+    let checkedSex;
+    
+    if (document.getElementById("optionsRadios1").checked){
+        checkedSex = "male";
+    } else if (document.getElementById("optionsRadios2").checked){
+        checkedSex = "female";
+    } else if (document.getElementById("optionsRadios3").checked){
+        checkedSex = "both";
+    }
+    
+    document.getElementById("generate-name").innerHTML = "";
+    
+    if (document.getElementById("flexCheckChecked1").checked){
+        if ((checkedSex == "male")||((checkedSex == "both")&&(rr===0))){
+            document.getElementById("generate-name").innerHTML = maleNames[rn]
+        } else if ((checkedSex == "female")||((checkedSex == "both")&&(rr===1))){
+            document.getElementById("generate-name").innerHTML = femaleNames[rn]
+        }
+    }
+    
+    if (document.getElementById("flexCheckChecked2").checked){
+        document.getElementById("generate-name").innerHTML += " " + surNames[rs1] + " " + surNames[rs2];
+    }
+    
+    if (document.getElementById("flexCheckChecked3").checked){
+        if ((checkedSex == "male")||((checkedSex == "both")&&(rr===0))){
+            document.getElementById("generate-img").src = maleImages[ri];
+        } else if ((checkedSex == "female")||((checkedSex == "both")&&(rr===1))){
+            document.getElementById("generate-img").src = femaleImages[ri];
+        }
+    } else {
+        document.getElementById("generate-img").src = "";
+    }
+}
